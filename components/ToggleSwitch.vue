eff6596ae6beafdeb4c9d38e5108ff662e291863
hectorstudio <5674062+hectorstudio@users.noreply.github.com>
2020-05-26 12:50:47 +0200

    Feat: Improve toggle component

--- components/ToggleSwitch.vue
+++ components/ToggleSwitch.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="toggle">
-        <input type="radio" id="monthly" value="monthly" name="toggle" checked />
-        <label for="monthly">Monthly</label>
-        <input type="radio" id="yearly" value="yearly" name="toggle" />
-        <label for="yearly">Yearly</label>
+        <fieldset class="toggle__fieldset">
+            <input type="radio" id="monthly" value="monthly" name="toggle" checked />
+            <label for="monthly">Monthly</label>
+            <input type="radio" id="yearly" value="yearly" name="toggle" />
+            <label for="yearly">Yearly</label>
+            <span class="toggle__switch"></span>
+        </fieldset>
     </div>
 </template>
 
