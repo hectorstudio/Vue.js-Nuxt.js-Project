d3fc4a3bbb9c73c9180d05509c069c2a782fd343
hectorstudio <5674062+hectorstudio@users.noreply.github.com>
2020-06-08 13:20:43 +0300

    Subscribe section

---
+++ components/Cards/PriceCardGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="pricecards">
+        <li class="pricecard" v-for="(card, index) in cards" :key="'logo-' + index">
+            <PriceCard :card="card" />
+        </li>
+    </ul>
+</template>
+
+<script>
+import PriceCard from "@/components/Cards/PriceCard";
+
+export default {
+    name: "PriceCardGrid",
+    components: {
+        PriceCard
+    },
+    props: {
+        cards: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
---
+++ components/Sections/SectionSubscribe.vue
@@ -0,0 +1,57 @@
+<template>
+    <section class="subscribe">
+        <div class="container container--content">
+            <div class="subscribe-outer">
+                <div class="subscribe-inner">
+                    <SectionTitle :title="title" :isCentered="false" :isInverted="true" />
+                    <div class="toggle-button-wrap">
+                        <ToggleSwitch />
+                    </div>
+                    <div class="price-table">
+                        <PriceCardGrid :cards="cards" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import SectionTitle from "@/components/SectionParts/SectionTitle";
+import ToggleSwitch from "@/components/ToggleSwitch";
+import PriceCardGrid from "@/components/Cards/PriceCardGrid";
+
+export default {
+    name: "SectionSubscribe",
+    components: {
+        SectionTitle,
+        ToggleSwitch,
+        PriceCardGrid
+    },
+    data() {
+        return {
+            title: "Subscribe",
+            cards: [
+                {
+                    title: "Freelancer",
+                    subtitle: "per month/person",
+                    currency: "$",
+                    price: "15",
+                    desc: "200 credits/month",
+                    buttontext: "Get Freelancer",
+                    buttonlink: "/"
+                },
+                {
+                    title: "Studio",
+                    subtitle: "per month/seat",
+                    currency: "$",
+                    price: "32",
+                    desc: "700 credits/month",
+                    buttontext: "Get Studio",
+                    buttonlink: "/"
+                }
+            ]
+        }
+    }
+}
+</script>
