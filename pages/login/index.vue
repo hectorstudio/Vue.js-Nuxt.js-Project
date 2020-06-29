95a3c2f09cb8feac79092197115b0d6b029c352c
hectorstudio <5674062+hectorstudio@users.noreply.github.com>
2020-06-29 13:43:12 +0200

    prep login fix

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="login">
     <div class="container container--content">
-      <div class="login__title">
-
-      </div>
+      <h1 class="login__title">{{ title }}</h1>
       <div class="login__btn-wrap">
         <div class="button-quaternary button-quaternary--blue">
           sign up by
           <b>facebook</b>
         </div>
         <div class="button-quaternary button-quaternary--salmon">
-          sing up by <b>google</b>
+          sing up by
+          <b>google</b>
         </div>
       </div>
       <div class="login__divider">
         <span>or</span>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      title: "Account Login",
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/pages/login.scss';
+@import "@/assets/scss/pages/login.scss";
 </style>
