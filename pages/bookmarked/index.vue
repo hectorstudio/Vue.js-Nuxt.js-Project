b2446859e7f843949642faa083656635e3d5999b
hectorstudio <5674062+hectorstudio@users.noreply.github.com>
2020-07-03 15:47:39 +0200

    Feat: Add bookmarked component

---
+++ pages/bookmarked/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section class="services">
+      <div class="container container--content">
+          Bookmarked
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import SectionTitle from "@/components/SectionParts/SectionTitle";
+import CardGrid from "@/components/Cards/CardGrid";
+import SectionFaq from "@/components/Sections/SectionFaq";
+
+export default {
+  name: "Tutorials",
+  components: {
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/pages/bookmarked.scss";
+</style>
--- pages/tutorials/index.vue
+++ pages/tutorials/index.vue
@@ -73,5 +73,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/scss/pages/login.scss";
+@import "@/assets/scss/pages/tutorials.scss";
 </style>
